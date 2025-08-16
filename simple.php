e44c72167744778b75a1cee50238050521634076
hipper-gif <sghrsho@gmail.com>
2025-08-16 10:23:51 +0900

    Update simple.php

--- simple.php
+++ simple.php
@@ -1,5 +1,7 @@
 <?php
-echo "Hello World";
+echo "Hello World from Smiley System";
 echo "<br>";
 echo "PHP Version: " . phpversion();
+echo "<br>";
+echo "Current Time: " . date('Y-m-d H:i:s');
 ?>
